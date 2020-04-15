d66e63abf7e0138d45b1eca40bf7789b5a9982a1
Aaron Ortega <aaortega18@gmail.com>
2020-04-15 11:05:58 -0500

    added docstring detailing approach to be taken for overlap calculation

--- src/calculate_area_overlap.py
+++ src/calculate_area_overlap.py
@@ -1,7 +1,23 @@
 """
-This script does the following:
-    1. Queries a tileset
-    2.
+This script calls Mapbox API and queries if a point is touching a geometry.
+
+What we did:
+    In main.py we constructed polygons that illustrate the distance one can
+    travel after 5 and 10 mins starting from a fridge location. These polygons
+    were all saved into a tileset called "chicago_milwaukee_data_polygons".
+
+What we want:
+    Now we want to find how many N polygons overlap a single point.
+    Note: The true value should be (N-2) because the point already has two
+    polygons that it centers (ie the ones built in main.py).
+
+Tools used:
+    Mapbox's Tilequery feature which builds a circle of radius R about a
+    point and returns a list of geometries that intersect the circle.
+
+How to calculate overlap:
+    If a polygon overlaps a point, then the distance from each other
+    will be zero. So we use pass R = 0 to find max overlap per point.
 """
 
 from mapbox import Tilequery
@@ -14,11 +30,11 @@ LONG = -87.595527
 LAYER = 'chicago_milwaukee_data_polygons'
 TILESET_ID = f'aortega04.{LAYER}'
 
-radius = 1.4 * 60 * 10  # Avg walk speed 1.4 (m/sec) * 60 (sec/min) * 10 (min) = 840 m (distance traveled in 10 min)
-
+# Distance between point and polygon overlap
+radius = 0
 
 tile_query = Tilequery(access_token=TOKEN)
-response = tile_query.tilequery(TILESET_ID, lon=LONG, lat=LAT, radius=radius)
+response = tile_query.tilequery(TILESET_ID, lon=LONG, lat=LAT, radius=radius, geometry='polygon', limit=50)
 
 if response.status_code == 200:
     print(response.json())
